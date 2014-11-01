6ee5908b7aff82533885b311b55cb72bb0c237ea
Mandy Huang <mandyxhuang@gmail.com>
2014-11-01 03:29:13 -0700

    I am a Pokemon master.

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,38 @@
+class PokemonsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @poke = Pokemon.new(pokemon_params)
+    @poke.update_attributes(:level => 1, :trainer_id => current_trainer.id, :health => 100)
+    saved = @poke.save
+    if saved
+      redirect_to trainer_path(@current_trainer.id)
+    else
+      #redirect_to new_path
+      flash[:error] = @pokemon.errors.full_messages.to_sentence
+    end
+  end
+
+  def capture
+  	@poke = Pokemon.find(params[:id])
+  	@poke.update_attributes(:trainer_id => current_trainer.id)
+  	#1/0
+  	redirect_to root_path
+  end
+
+  def damage
+    @poke = Pokemon.find(params[:id])
+    @poke.update_attributes(:health => @poke.health-10)
+    if @poke.health <= 0
+    	@poke.destroy
+    end
+    #1/0
+    redirect_to trainer_path(@poke.trainer_id)
+  end
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+  patch '/pokemons/:id', to:'pokemons#damage', as:'damage'
+  patch '/pokemons/:id', to:'pokemons#capture', as:'capture'
+  get 'pokemons/new', to:'pokemons#new', as:'new'
+  post '/pokemons', to:'pokemons#create', as:'create'
+
 end
